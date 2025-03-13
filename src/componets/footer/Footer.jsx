52c555898990096a79e0205edd408be93094eaf2
bkay-stack <kevinbest1900@gmail.com>
2025-03-13 09:57:36 +0100

    footer

--- src/componets/footer/Footer.jsx
+++ src/componets/footer/Footer.jsx
@@ -1,10 +1,75 @@
 import React from "react";
 import "./footer.style.css";
-
+import fbImg from "../../assets/images/icon-facebook.svg";
+import twImg from "../../assets/images/icon-twitter.svg";
+import pinImg from "../../assets/images/icon-pinterest.svg";
+import instaImg from "../../assets/images/icon-instagram.svg";
 const Footer = () => {
   return (
-    <footer className="footer-container">
-      <h1>Footer</h1>
+    <footer>
+      <div className="footer-wrap">
+        <div className="footer-logo">
+          <h1>Shortly</h1>
+        </div>
+        <div className="footer-links">
+          <h1>Features</h1>
+          <ul className="footer-list">
+            <li>
+              <a href="#"> Link Shortening</a>
+            </li>
+            <li>
+              <a href="#"> Branded Links</a>
+            </li>
+            <li>
+              <a href="#">Analytics</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-links">
+          <h1>Resources</h1>
+          <ul className="footer-list">
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#"> Developers </a>
+            </li>
+            <li>
+              <a href="#">Support</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-links">
+          <h1>Company</h1>
+          <ul className="footer-list">
+            <li>
+              <a href="#"> About</a>
+            </li>
+            <li>
+              <a href="#"> Our Team</a>
+            </li>
+            <li>
+              <a href="#"> Careers</a>
+            </li>
+          </ul>
+        </div>
+        <div className="footer-links ">
+          <ul className="footer-list icon-footer">
+            <li>
+              <img src={fbImg} alt="" />
+            </li>
+            <li>
+              <img src={twImg} alt="" />
+            </li>
+            <li>
+              <img src={pinImg} alt="" />
+            </li>
+            <li>
+              <img src={instaImg} alt="" />
+            </li>
+          </ul>
+        </div>
+      </div>
     </footer>
   );
 };
